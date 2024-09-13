79e595c468a8bdcc47ba308bbc67e89863933e2d
fubuki4649 <git@franktao.com>
2024-09-12 22:29:46 -0400

    working heightmap rendering system

--- script/main.js
+++ script/main.js
@@ -1,15 +1,20 @@
 import {init as initScene, addTriangle, renderScene} from './render.js'
 import init, {  } from '../terrain-gen/pkg/terrain_gen.js';
+import {test} from "./scene.js";
 
 
 initScene();
 
 await init();
 
-addTriangle([
-    0.0, 0.0, 0,
-    1.0, 0.0, 0,
-    0.0,  0.5, 1.0
-]);
+// addTriangle([
+//     0.0, 0.0, 0,
+//     1.0, 0.0, 0,
+//     0.0,  0.5, 1.0
+// ]);
 
 renderScene();
+
+
+// TODO: everything below is for testing, remove later
+test();
--- script/scene.js
+++ script/scene.js
@@ -1,11 +1,73 @@
+import {addTriangle, renderScene} from "./render.js";
 
+let mapWidth = 0;
+let mapLength = 0;
+
+let heightMap = new Float32Array(0);
+
+function getHeightValue(x, y) {
+    return heightMap[y * mapLength + x];
+}
 
 // Calls WASM code to generate the terrain
-export function generateTerrain() {
+export function generateTerrain(width, length) {
+
+    mapWidth = width;
+    mapLength = length;
+
     // Call WASM code to generate scene
+}
+
+export function retrieveTerrain() {
+
+    // Call WASM code to retrieve mesh data
+
+}
+
+
+// TODO: remove this later, its used for testing
+export function test() {
+
+    mapWidth = 4
+    mapLength = 4
+
+    heightMap = new Float32Array([
+        1, 2, 3, 4,
+        2, 3, 4, 5,
+        3, 4, 5, 6,
+        4, 5, 6, 7
+    ])
+
+    showTerrain();
+
 }
 
 // Loads terrain data into scene
 export function showTerrain() {
-    // Call WASM code to add triangles
+
+    for(let col = 0; col < mapWidth - 1; col++) {
+
+        for(let row = 0; row < mapLength - 1; row ++) {
+
+            const topLeft = getHeightValue(row, col), topRight = getHeightValue(row + 1, col),
+                bottomLeft = getHeightValue(row, col + 1), bottomRight = getHeightValue(row + 1, col + 1);
+
+            addTriangle([
+                row, col, topLeft,
+                row + 1, col, topRight,
+                row,  col + 1, bottomLeft
+            ])
+
+            addTriangle([
+                row + 1, col + 1, bottomRight,
+                row + 1, col, topRight,
+                row,  col + 1, bottomLeft
+            ])
+
+        }
+
+    }
+
+    renderScene();
+
 }
